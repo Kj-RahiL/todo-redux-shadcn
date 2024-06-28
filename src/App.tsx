1fd402043cfeb5330a970b68ea6102f5537c3b6f
mir-hussain <mir.hm110@gmail.com>
2024-06-28 23:21:56 +0600

    fix: bun removed and yarn added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-500">Test</h1>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
